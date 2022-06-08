7fef2a4e27bfb960b303bd07ebd5df93fa656d06
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-06-08 15:51:35 +0200

    Add todolist code

--- src/index.js
+++ src/index.js
@@ -1,13 +1,46 @@
-import _ from 'lodash';
+// Imports
+import './index.css';
+/*eslint-disable*/
+import addElem from './modules/addElement.js';
+import TaskList from './modules/classTaskList.js';
+import refreshList from './modules/refresh.js';
 
-function component() {
-  const element = document.createElement('div');
+const taskList = new TaskList();
 
-  // Lodash, currently included via a script, is required for this line to work
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+// DOM
+const mainContainer = document.querySelector('.todo-list-container');
 
-  return element;
-}
+// Header (Title and input)
+mainContainer.innerHTML = `<div class="row">
+<h1>Today's To Do</h1>
+<i class="fa-solid fa-rotate fa-lg font-awesome-icon"></i>
+</div>`;
+const inputContainer = addElem('form', [], mainContainer);
+const inputText = addElem('input', ['input-add-task'], inputContainer);
+inputText.setAttribute('placeholder', 'Add to your list...');
+addElem('i', ['fa-solid', 'fa-arrow-right-to-bracket', 'fa-sm', 'font-awesome-icon'], inputContainer);
+// Main (list)
 
-document.body.appendChild(component());
+const listContainer = addElem('div', [], mainContainer);
+// Bottom (button)
+const clearBtn = addElem('button', ['button'], mainContainer);
+clearBtn.textContent = 'Clear all completed';
+
+// Input
+inputContainer.onsubmit = (e) => {
+  e.preventDefault();
+  taskList.addTask(inputText.value);
+
+  inputContainer.reset();
+  refreshList(taskList, listContainer);
+};
+
+// clear button
+clearBtn.onclick = () => {
+  // console.log('clear');
+  taskList.clearCompleted();
+  refreshList(taskList, listContainer);
+};
+
+// On load
+refreshList(taskList, listContainer);
